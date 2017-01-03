c0ab2c55ab798ad140711871d0c266b489b93547
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-01-03 15:59:48 -0500

    is uniq.

--- ch1_arrays_and_strings/is_unique.rb
+++ ch1_arrays_and_strings/is_unique.rb
@@ -1,3 +1,6 @@
+# Implement an algorithm to determine if a string has all unique characters.
+
 def unique?(str)
+  str.each_char { |char| return false if str.count(char) > 1 }
   true
 end
--- spec/ch1_srrays_and_strings/is_unique_spec.rb
+++ spec/ch1_srrays_and_strings/is_unique_spec.rb
@@ -2,6 +2,10 @@ require_relative "../../ch1_arrays_and_strings/is_unique"
 
 describe "unique?" do
   it "returns true when characters are uniq" do
-    expect(unique?("foo")).to eq true
+    expect(unique?("qwerty")).to eq true
+  end
+
+  it "returns false when characters aren't uniq" do
+    expect(unique?("yqwerty")).to eq false
   end
 end
