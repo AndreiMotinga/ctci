a1e6dd3a07b36bac1fdb3970c62b41e154a5d124
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-01-09 19:42:32 -0500

    rewrite in OO

--- arrays_and_strings/zero_matrix.rb
+++ arrays_and_strings/zero_matrix.rb
@@ -1,18 +1,33 @@
-def zero_matrix(input)
-  rows = []
-  columns = []
+# Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
 
-  input.each_with_index do |line, line_i|
-    if line.include? 0
-      rows << line_i
-      line.each_with_index { |el, el_i|  columns << el_i if el == 0 }
+class Matrix
+  attr_reader :matrix
+
+  def initialize(input)
+    @matrix = input
+  end
+
+  def zero
+    zero = matrix.map(&:dup)
+    zero.each_with_index do |line, line_i|
+      line.map! { |el| el = 0 } if rows_with_zero.include? line_i
+      line.each_index { |i| line[i] = 0 if columns_with_zero.include? i }
     end
   end
 
-  input.each_with_index do |line, line_i|
-    line.map! { |el| el = 0 } if rows.include? line_i
-    line.each_index { |i| line[i] = 0 if columns.include? i }
+  private
+
+  def columns_with_zero
+    columns = []
+    matrix.each_with_index do |line, line_i|
+      line.each_with_index { |el, el_i|  columns << el_i if el == 0 }
+    end
+    columns
   end
 
-  input
+  def rows_with_zero
+    rows = []
+    matrix.each_with_index { |line, i| rows << i if line.include? 0 }
+    rows
+  end
 end
--- spec/arrays_and_strings/zero_matrix_spec.rb
+++ spec/arrays_and_strings/zero_matrix_spec.rb
@@ -1,15 +1,31 @@
 require_relative "../../arrays_and_strings/zero_matrix"
 
-describe "#zero_matrix" do
-  it "sets rows and columns containing 0 to zeros" do
-    input = [[1, 2, 3],
-             [1, 0, 1],
-             [1, 1, 1]]
+describe Matrix do
+  describe "#zero" do
+    it "sets rows and columns containing 0 to zeros" do
+      input = [[1, 2, 3],
+               [1, 0, 1],
+               [1, 1, 1]]
 
-    result = [[1, 0, 3],
-              [0, 0, 0],
-              [1, 0, 1]]
+      result = [[1, 0, 3],
+                [0, 0, 0],
+                [1, 0, 1]]
 
-    expect(zero_matrix(input)).to eq result
+      expect(Matrix.new(input).zero).to eq result
+    end
+  end
+
+  describe "#zero" do
+    it "sets rows and columns containing 0 to zeros" do
+      input = [[0, 2, 3],
+               [1, 0, 1],
+               [1, 1, 1]]
+
+      result = [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 1]]
+
+      expect(Matrix.new(input).zero).to eq result
+    end
   end
 end
