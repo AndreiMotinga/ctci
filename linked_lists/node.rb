63c0fded4ac1e8999ce97cc64a913f006edd58dd
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-01-10 16:24:49 -0500

    add .generate_nodes and #print_nodes for testing ease

--- linked_lists/node.rb
+++ linked_lists/node.rb
@@ -8,5 +8,28 @@ class Node
   # ex: remove duplicates from an unsorted linked list.
   # def remove_dups
   #   duplicates = []
+  #
   # end
+
+  def print_nodes
+    string = "#{self.value}"
+    current = self.next
+    while current do
+      string += " -> #{current.value}"
+      current = current.next
+    end
+    string
+  end
+
+  def self.generate_nodes(values)
+    current = head = new values.first
+    values.each_with_index do |v, i|
+      next if i == 0
+      next_node = new v
+      next_node.prev = current
+      current.next = next_node
+      current = next_node
+    end
+    head
+  end
 end
--- spec/linked_lists/node_spec.rb
+++ spec/linked_lists/node_spec.rb
@@ -35,6 +35,33 @@ describe Node do
       expect(node.prev).to eq prev
     end
   end
+
+  # ex: remove duplicates from an unsorted linked list.
+  # describe "#remove_dups" do
+  #   it "removes nodes with duplicate values" do
+  #     head = Node.generate_nodes([1,2,2,2,3,4,3,5])
+  #
+  #     expect(head)
+  #   end
+  # end
+
+  describe ".generate_nodes" do
+    it "generates nodes from array of values returning head" do
+      head = Node.generate_nodes([1,2,2,2,3,4,3,5])
+
+      expect(head.value).to eq 1
+      expect(head.next.value).to eq 2
+      expect(head.next.prev.value).to eq 1
+    end
+  end
+
+  describe "#print_nodes" do
+    it "print next nodes values joined with ' -> '" do
+      head = Node.generate_nodes([1,3,5])
+
+      result = "1 -> 3 -> 5"
+
+      expect(head.print_nodes).to eq result
     end
   end
 end
